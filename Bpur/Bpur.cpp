4a983df9dc0249a108a40845da6915d079470ccf
Bogdan Gligorijevic <bogdangligorijevic3@gmail.com>
2020-12-17 13:28:06 +0100

    First prettiness commit

--- Bpur/Bpur.cpp
+++ Bpur/Bpur.cpp
@@ -4,8 +4,26 @@
 #include <string>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#define fs std::filesystem
+namespace fs = std::filesystem;
 SOCKET sock = INVALID_SOCKET;
+
+char* getStringVar(const char* name)
+{
+	size_t varsize;
+	char *var;
+	getenv_s(&varsize, NULL, 0, name);
+	if (varsize == 0)
+	{
+		throw std::string(name) + " not set!\n";
+	}
+	var = (char*)malloc(varsize * sizeof(char));
+	if (!var)
+	{
+		throw "Failed to allocate memory for " + std::string(name) + "\n";
+	}
+	getenv_s(&varsize, var, varsize, name);
+	return var;
+}
 
 int main(int argc, char** argv)
 {
@@ -32,8 +50,6 @@ int main(int argc, char** argv)
 	}
 	for (ainfo; ainfo != NULL; ainfo = ainfo->ai_next)
 	{
-
-		// Create a SOCKET for connecting to server
 		sock = socket(ainfo->ai_family, ainfo->ai_socktype, ainfo->ai_protocol);
 		if (sock == INVALID_SOCKET)
 		{
@@ -41,7 +57,6 @@ int main(int argc, char** argv)
 			return 1;
 		}
 
-		// Connect to server.
 		temp = connect(sock, ainfo->ai_addr, (int)ainfo->ai_addrlen);
 		if (temp == SOCKET_ERROR)
 		{
@@ -60,64 +75,35 @@ int main(int argc, char** argv)
 	}
 
 	char* password;
-	size_t envsize;
-	getenv_s(&envsize, NULL, 0, "BPUR_PASSWORD");
-	if (envsize == 0)
+	char* username;
+	char* channel;
+	try
 	{
-		printf("BPUR_PASSWORD not set!\n");
+		password = getStringVar("BPUR_PASSWORD");
+		username = getStringVar("BPUR_USERNAME");
+		channel  = getStringVar("BPUR_CHANNEL");
+	}
+	catch (std::string e)
+	{
+		std::cout << e;
 		return 1;
 	}
-	password = (char*)malloc(envsize * sizeof(char));
-	if (!password)
-	{
-		printf("Failed to allocate memory!\n");
-		return 1;
-	}
-	getenv_s(&envsize, password, envsize, "BPUR_PASSWORD");
 	std::string message = "PASS ";
 	message += password;
 	message += "\r\n";
-	send(sock, message.c_str(), message.length(), 0);
+	send(sock, message.c_str(), (int)message.length(), 0);
 
-	char* username;
-	getenv_s(&envsize, NULL, 0, "BPUR_USERNAME");
-	if (envsize == 0)
-	{
-		printf("BPUR_USERNAME not set!\n");
-		return 1;
-	}
-	username = (char*)malloc(envsize * sizeof(char));
-	if (!username)
-	{
-		printf("Failed to allocate memory!\n");
-		return 1;
-	}
-	getenv_s(&envsize, username, envsize, "BPUR_USERNAME");
 	message = "NICK ";
 	message += username;
 	message += "\r\n";
-	send(sock, message.c_str(), message.length(), 0);
+	send(sock, message.c_str(), (int)message.length(), 0);
 
-	char* channel;
-	getenv_s(&envsize, NULL, 0, "BPUR_CHANNEL");
-	if (envsize == 0)
-	{
-		printf("BPUR_CHANNEL not set!\n");
-		return 1;
-	}
-	channel = (char*)malloc(envsize * sizeof(char));
-	if (!channel)
-	{
-		printf("Failed to allocate memory!\n");
-		return 1;
-	}
-	getenv_s(&envsize, channel, envsize, "BPUR_CHANNEL");
 	message = "JOIN #";
 	message += channel;
 	message += "\r\n";
-	send(sock, message.c_str(), message.length(), 0);
+	send(sock, message.c_str(), (int)message.length(), 0);
 
-	//fileread code
+	//banlist code
 	if (argv[1] && fs::exists(fs::path(argv[1])))
 	{
 		std::ifstream list(argv[1]);
@@ -126,13 +112,13 @@ int main(int argc, char** argv)
 			message = "/ban " + line + "\r\n";
 			message = std::string(":") + username + std::string("!") + username + std::string("@") + username +
 				std::string(".tmi.twitch.tv PRIVMSG #") + channel + std::string(" :") + message;
-			//message = std::string("PRIVMSG #") + channel + std::string(" :") + message;
-			send(sock, message.c_str(), message.length(), 0);
+			send(sock, message.c_str(), (int)message.length(), 0);
 			Sleep(340);
 		}
 	}
 	else
 		std::cout << "Invalid list file.\n";
+
 	free(password);
 	free(username);
 	free(channel);
